2eb5c8088f8edb6eef8c0e991621c5d7726f8abe
JooYoungHoon <dudgns35111802@gmail.com>
2023-02-17 19:34:48 +0900

    w03_02 별찍기 완성

--- younghoon/Week03/no2_10994.py
+++ younghoon/Week03/no2_10994.py
@@ -2,30 +2,38 @@
 
 import sys
 
-def star(N, line):
-    if N == 1:
-        print("*")
-    else:
-        stars = ['*' for _ in range(4*N-3)]
-        
-        # 처음과 마지막 줄
-        if line == 0 or line == N-1:
-            print(''.join(stars))
-        
-        # 홀수 번째 줄 and 중간지점 전
-        elif line % 2 != 0 and line < 2*N-2:
-            stars[]
-        
-        # 짝수 번째 줄
-        else:
-            pass
-            
-
 if __name__ == "__main__":
     # 입력
     N = int(input())
 
     # 별그리기
-    for i in range(4*N-3):
-        star(N, i)
+    if N == 1:
+        print("*")
+    else:
+        stars = ['*' for _ in range(4*N-3)]
+        last = 4*N - 3
+        mid = 2*N - 2
+        for line in range(4*N-3):
+            # 처음 줄
+            if line == 0:
+                print(''.join(stars))
+                
+            # 중간 이전 and 홀수 번째 줄
+            elif line <= 2*N - 2 and line % 2 != 0:
+                stars[line:last-line] = " "*(last-2*line)
+                print(''.join(stars))
+
+            # 중간 이전 and 짝수 번째 줄
+            elif line <= 2*N - 2 and line % 2 == 0:
+                stars[line:last-line] = "*"*(last-2*line)
+                print(''.join(stars))
             
+            # 중간 이후 and 홀수 번째 줄
+            elif line > 2*N - 2 and line % 2 != 0:
+                stars[2*mid-line:line] = " "*(2*(line-mid))
+                print(''.join(stars))
+            
+            # 중간 이후 and 짝수 번째 줄
+            elif line > 2*N -2 and line % 2 == 0:
+                stars[2*mid-line:line] = "*"*(2*(line-mid))
+                print(''.join(stars))
